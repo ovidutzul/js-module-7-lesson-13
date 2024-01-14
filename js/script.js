29150fa84371a9c7da9a4caa891e3932aaec013e
ovidutzul <142990412+ovidutzul@users.noreply.github.com>
2024-01-14 17:05:51 +0200

    Event propagation exercises

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// const articleElement = document.getElementById("myArticle");
+// const divElement = document.getElementById("myDiv");
+// const buttonElement = document.getElementById("myButton");
+
+// articleElement.addEventListener("click", (e) => {
+//   console.log("Clicked on");
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+// });
+
+// divElement.addEventListener("click", (e) => {
+//   console.log("Clicked on");
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+// });
+
+// buttonElement.addEventListener("click", (e) => {
+//   console.log("Clicked on");
+//   console.log(e.target);
+//   console.log(e.currentTarget);
+// });
